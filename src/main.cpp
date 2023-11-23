827fbf4d6c3c4936c6e7cffd2b5743421e140c12
LAPTOPRICK2\rickv <rickvanginkel@yahoo.com>
2023-11-23 16:38:02 +0100

    joystick display control

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,10 @@ const uint8_t ball_radius = 10;//testing
 bool testBool1 = true;//testing 
 bool testBool2 = true;//testing
 
+uint8_t playerPosX;
+uint8_t playerPosY;
+
+uint8_t NunChuckPosition[4];
 
 
 
@@ -82,14 +86,16 @@ void updateBallPos(){//testing
 
 void drawPlayer(uint8_t x, uint8_t y){
     tft.fillRect(x,y,25,25,ILI9341_DARKCYAN);
+    playerPosX = x;
+    playerPosY = y;
 }
 
 void drawPath(uint8_t x, uint8_t y){
     tft.fillRect(x,y,25,25,ILI9341_BLACK);
 }
 
-void movePlayer(uint8_t oldX,uint8_t oldY,uint8_t newX,uint8_t newY){
-    drawPath(oldX,oldY);
+void movePlayer(uint8_t newX,uint8_t newY){
+    drawPath(playerPosX,playerPosY);
     drawPlayer(newX,newY);
 }
 
@@ -98,63 +104,111 @@ void drawLevel(){
         drawPath(i,200);
     }
 }
+
+bool setupNunchuck(){
+    if(!Nunchuk.begin(nunchuk_ID)){
+        Serial.println("******** No nunchuk found");
+        Serial.flush();
+        return(false);
+    }
+    Serial.print("-------- Nunchuk with Id: ");
+	Serial.println(Nunchuk.id);
+    return true;
+}
+
+
+void getNunchukPosition(){
+    if(!Nunchuk.getState(nunchuk_ID)){
+        return;
+    } 
+
+    NunChuckPosition[0] = Nunchuk.state.joy_x_axis;
+    NunChuckPosition[1] = Nunchuk.state.joy_y_axis;
+    NunChuckPosition[2] = Nunchuk.state.c_button;
+    NunChuckPosition[3] = Nunchuk.state.z_button;
+    
+}
+
+
 int main(){
 
 
     sei();
     Serial.begin(BAUDRATE);
     Wire.begin();
-if(!Nunchuk.begin(nunchuk_ID)){
-    Serial.println("******** No nunchuk found");
-	Serial.flush();
-	return(1);
-}
-    Serial.print("-------- Nunchuk with Id: ");
-	Serial.println(Nunchuk.id);
+
+    if(!setupNunchuck()){return 0;}
+    setupDisplay();
+
+
+    tft.fillScreen(ILI9341_RED);
+    drawLevel();
+    drawPlayer(100,200);
+
+    /*
     while(1){
         if(!Nunchuk.getState(nunchuk_ID)){
             return (1);
         } 
+
+
         int x = Nunchuk.state.joy_x_axis;
 	    int y = Nunchuk.state.joy_y_axis;
         int c = Nunchuk.state.c_button;
         int z = Nunchuk.state.z_button;
         const char* uitkomst[4];
         if(x != 128){
-            uitkomst[0] = x<128 ? "links-" : "rechts-";} else{
+            uitkomst[0] = x<128 ? "links-" : "rechts-";}
+        else{
             uitkomst[0] = "midden-";
-    }
-    if(y != 128){
-            uitkomst[1] = y>128 ? "boven-" : "onder-";} else{
-            uitkomst[1] = "midden-";
-    }
-    uitkomst[2] = c ? "cin-" : "cout-";
-    uitkomst[3] = z ? "zin" : "zout";
-    for(int i = 0;i<4;i++){
-        Serial.print(uitkomst[i]);
-    }
-    Serial.println(" ");
-//}
+        }
+        if(y != 128){
+                uitkomst[1] = y>128 ? "boven-" : "onder-";} 
+        else{
+                uitkomst[1] = "midden-";
+        }
+        uitkomst[2] = c ? "cin-" : "cout-";
+        uitkomst[3] = z ? "zin" : "zout";
+        for(int i = 0;i<4;i++){
+            Serial.print(uitkomst[i]);
+        }
+        Serial.println(" ");
+    }*/
 
-    setupDisplay();
-    tft.fillScreen(ILI9341_RED);
-    // tft.fillRect(100,100,100,100,ILI9341_ORANGE);
-    drawLevel();
-    drawPlayer(100,200);
+
 
     while(true){
+        int start = millis();
+        getNunchukPosition();
+
+        // Serial.print(NunChuckPosition[0]);
+        // Serial.print(":");
+        // Serial.print(NunChuckPosition[1]);
+        // Serial.print(":");
+        // Serial.print(NunChuckPosition[2]);
+        // Serial.print(":");
+        // Serial.print(NunChuckPosition[3]);
+        // Serial.println(" ");
+
+
         
-        //scrappy bouncing ball
-        // tft.fillScreen(ILI9341_BLUE);
-        movePlayer(100,200,300,100);
-        _delay_ms(50);
-        movePlayer(300,100,100,200);
-        _delay_ms(50);
+        movePlayer(NunChuckPosition[1],NunChuckPosition[0]);
+        int end = millis();
+        Serial.println(end-start);
+        // _delay_ms(50);
+        // movePlayer(400,300);
+        // _delay_ms(50);
+
+        // movePlayer(200,200);
+        // _delay_ms(50);
+
+        // movePlayer(100,200);
+        // _delay_ms(50);
 
 
 
-        updateBallPos();
+        // updateBallPos();
 
     }  
-return 0;
+    return 0;
 }
