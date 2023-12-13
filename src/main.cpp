99a78221af036de71f578171eef7415a17a41d5b
jurreH <62851059+Soul-Soil@users.noreply.github.com>
2023-12-13 14:29:02 +0100

    volledige highscore layout, geprepareerd voor mergen

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,42 @@
 #include <EEPROM.h>
 #include <Arduino.h>
-#include "main.h"
 #include "display/Adafruit-GFX/Adafruit_GFX.h"
 #include "display/Adafruit_ILI9341.h"
 #include <Fonts/PressStart2P_vaV76pt7b.h>
 
-char name [10][6] = {};
-uint32_t score [10] = {};
-int pos[10] = {0,1,2,3,4,5,6,7,8,9};
+#define TFT_DC 9 
+#define TFT_CS 10
+#define BACKGROUND ILI9341_BLACK //Background color
+#define HIGHSCORECOLOR ILI9341_YELLOW //color of highscore text
+#define PLAYERNAME "Jurre" //player name for adding highscores
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS,TFT_DC);
+
+char nameList [10][6] = {}; //list to store playernames from EEPROM
+uint32_t scoreList [10] = {};//list to store scores from EEPROM
+int posList[10] = {0,1,2,3,4,5,6,7,8,9}; //list to keep track of indexes in EEPROM
+
+void HighScorePage();
 bool setupHighScore();
 void getScore();
 void setupScore();
 void printHighScore(char names[10][6],uint32_t scores[10]);
-void sort(char name[10][6],uint32_t score[10], int pos[10]);
-void addScore(char naam[6], uint32_t points);
-#define TFT_DC 9
-#define TFT_CS 10
-#define BACKGROUND ILI9341_BLACK
-#define TEXTCOLOR ILI9341_YELLOW
-#define NAAM "Jurre"
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS,TFT_DC);
-bool setupDisplay(){
-  tft.begin();
-  tft.setRotation(1);
-  return true;
-}
-int main(){
-  setupDisplay();
+void sort(char names[10][6],uint32_t scores[10], int pos[10]);
+void addScore(char name[6], uint32_t points);
+//function to call when going to highscore page
+void HighScorePage(){
   setupHighScore();
   getScore();
-  printHighScore(name,score);
-  return 1;
+  printHighScore(nameList,scoreList);
 }
+//clears the screen, sets the font and text color 
 bool setupHighScore(){
   tft.fillScreen(BACKGROUND);
   tft.setFont(&PressStart2P_vaV76pt7b);
-  tft.setTextColor(TEXTCOLOR);
+  tft.setTextColor(HIGHSCORECOLOR);
   return 1;
 }
+//gets the EEPROM data, should only be called once every time the arduino starts up
 void getScore(){
   char temp[6];
   uint32_t temp2;
@@ -45,15 +44,16 @@ void getScore(){
     EEPROM.get(i*10,temp);
     EEPROM.get(i*10+6,temp2);
     for(int j = 0;j<6;j++){
-      name[i][j] = temp[j];
+      nameList[i][j] = temp[j];
     }
-    score[i] = temp2;
+    scoreList[i] = temp2;
   }
-  sort(name,score,pos);
+  sort(nameList,scoreList,posList);
 }
+//prints the highscores, uses the scoreList and nameList
 void printHighScore(char names[10][6],uint32_t scores[10]){
   tft.fillScreen(BACKGROUND);
-  sort(name,score,pos);
+  sort(nameList,scoreList,posList);
   tft.setCursor(110,25);
   tft.println("Highscore");
   char bufHS[64];
@@ -63,12 +63,13 @@ void printHighScore(char names[10][6],uint32_t scores[10]){
     for(int j = 0;j<6;j++){
       tempN[j] = names[i][j];
     }
-    tempS = score[i];
+    tempS = scoreList[i];
     sprintf(bufHS,"%2i %-6s %10lu",10-i,tempN,tempS);
     tft.setCursor(50,200-i*15);
     tft.println(bufHS);
   }
 }
+//sorts the highscores so that they get called in the correct order, uses insertion sort
 void sort(char names[10][6],uint32_t scores[10],int pos[10]){
     char temp1[6] = {};
     uint32_t temp2;
@@ -79,23 +80,24 @@ void sort(char names[10][6],uint32_t scores[10],int pos[10]){
             temp1[i] = names[p][i];
         }
         temp2 = scores[p];
-        temp3 = pos[p];
+        temp3 = posList[p];
     for(j = p;j> 0 && temp2<scores[j-1];j--){
         scores[j] = scores[j-1];
-        pos[j] = pos[j-1];
+        posList[j] = posList[j-1];
     
         for(int i = 0;i<6;i++){
             names[j][i] = names[j-1][i];
         }
     }
     scores[j] = temp2;
-    pos[j] = temp3;
+    posList[j] = temp3;
     for(int i = 0;i<6;i++){
             names[j][i] = temp1[i];
         }
     }
 
   }
+//fills EEPROM with mock scores
 void setupScore(){
   char names[10][6] = {"henk","kees","klaas","piet","jan","wilem","jurre","andor","rick","jay"};
   uint32_t scores[10] ={1000000,1000,8000,4000,3000,6000,2000,7000,9000,5000};
@@ -106,13 +108,14 @@ void setupScore(){
     EEPROM.put(i*10+6,scores[i]);
   }
 }
-void addScore(char naam[6], uint32_t points){
-  if(points > score[0]){
-    score[0] = points;
+//adds a new score to the EEPROM, also add it to scoreList and nameList so you dont have to read all the EEPROM data again
+void addScore(char name[6], uint32_t points){
+  if(points > scoreList[0]){
+    scoreList[0] = points;
     for(int j = 0;j<6;j++){
-    name[0][j] = naam[j];
-    EEPROM.put(pos[0]*10+j,naam[j]);
+    nameList[0][j] = name[j];
+    EEPROM.put(posList[0]*10+j,name[j]);
     }
-    EEPROM.put(pos[0]*10+6,points);
+    EEPROM.put(posList[0]*10+6,points);
   }  
 }
